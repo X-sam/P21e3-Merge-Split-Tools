e858f55bd4c57d4516a00b743380a19f45646d6b
Sam-son <bonfas@rpi.edu>
2014-06-06 13:31:56 -0400

    Cleaned ARMSplit up. Refactored names, moved things to functions, added command line parser, etc. It's all real nice now.

--- ARMSplit/ARMSplit.cxx
+++ ARMSplit/ARMSplit.cxx
@@ -15,8 +15,83 @@
 #pragma comment(lib,"stpcad_stix.lib")
 
 void handleAggregate(RoseObject * obj);
+void handleEntity(RoseObject * obj);
 
-void addRefAndAnchor(RoseObject * obj, RoseDesign * ProdOut, RoseDesign * master, std::string dir=""){ //obj from output file, and master fiel for putting refs into
+void MakeReferencesAndAnchors(RoseDesign * source, RoseDesign * destination);
+void addRefAndAnchor(RoseObject * obj, RoseDesign * ProdOut, RoseDesign * master, std::string dir = "");
+
+void MoveGeometry(RoseDesign * source, RoseDesign * dest);
+
+int main(int argc, char* argv[])
+{
+
+	stplib_init();	// initialize merged cad library
+	
+	if (argc < 3)
+	{
+		std::cout << "Usage: " << argv[0] << " [Input.stp] [OutputMasterFile.stp]\n";
+	}
+
+	FILE *out;
+	out = fopen("log.txt", "w");
+	ROSE.error_reporter()->error_file(out);
+	RoseP21Writer::max_spec_version(PART21_ED3);	//We need to use Part21 Edition 3 otherwise references won't be handled properly.
+	ST_MODULE_FORCE_LOAD();
+
+	//##################### INITILIZE DESIGNS #####################
+	RoseDesign *des = ROSE.useDesign(argv[1]);
+	if (!des)
+	{
+		std::cout << "Error reading input file.\n";
+		return EXIT_FAILURE;
+	}
+	des->saveAs(argv[2]);
+	RoseDesign *MainOutput= ROSE.useDesign(argv[2]);
+	if (!MainOutput)
+	{
+		std::cout << "Error opening output file.\n";
+		return EXIT_FAILURE;
+	}
+	RoseDesign *geo = pnew RoseDesign;
+	std::string geoname(MainOutput->name());
+	geoname.append("_Geometry");
+	geo->name(geoname.c_str());
+	geo->save();
+	if (!geo)
+	{
+		std::cout << "Error opening geometry output file for writing.\n";
+		return EXIT_FAILURE;
+	}
+	stix_tag_units(MainOutput);
+	ARMpopulate(MainOutput);
+	//#############################################################
+
+	//STModule
+	rose_compute_backptrs(MainOutput);
+
+	MoveGeometry(MainOutput, geo);
+
+	MakeReferencesAndAnchors(MainOutput, geo);
+
+	update_uri_forwarding(MainOutput);
+
+	rose_mark_end();
+
+	ARMgc(MainOutput);
+	ARMgc(geo);
+	rose_empty_trash();
+
+	geo->save();
+	ARMsave(geo);
+
+	MainOutput->save();
+	ARMsave(MainOutput);
+
+	return 0;
+}
+
+
+void addRefAndAnchor(RoseObject * obj, RoseDesign * ProdOut, RoseDesign * master, std::string dir){ //obj from output file, and master fiel for putting refs into
 	std::string ProdOutName;
 	ProdOutName.append(obj->domain()->name());
 	ProdOutName.append("_split_item");
@@ -52,8 +127,8 @@ void handleEntity(RoseObject * obj)
 		}
 		if (att->isAggregate())
 		{
-		handleAggregate(obj->getObject(att));
-		continue;
+			handleAggregate(obj->getObject(att));
+			continue;
 		}
 		if (!childobj) continue;
 		if (childobj->design() != obj->design() && !rose_is_marked(childobj))
@@ -89,104 +164,59 @@ void handleAggregate(RoseObject * obj)
 	}
 }
 
-int main(int argc, char* argv[])
+void MakeReferencesAndAnchors(RoseDesign * source, RoseDesign * destination)
 {
-	
-	stplib_init();	// initialize merged cad library
-	//rose_p28_init();	// support xml read/write
-	
-	FILE *out;
-	out = fopen("log.txt", "w");
-	ROSE.error_reporter()->error_file(out);
-	RoseP21Writer::max_spec_version(PART21_ED3);	//We need to use Part21 Edition 3 otherwise references won't be handled properly.
-	ST_MODULE_FORCE_LOAD();
-
-//##################### INITILIZE DESIGNS #####################
-	printf("Reading file '%s'\n", "sp3-boxy_fmt_original.stp");
-	// Read a STEP file and dimension the workpiece in that file
-	RoseDesign *des = ROSE.useDesign("sp3-boxy_fmt_original.stp");
-	des->saveAs("PMI.stp");
-	RoseDesign *PMI = ROSE.useDesign("PMI.stp");
-	//PMI->name(std::string(des->name() + std::string("_PMI")).c_str()); //Breaks URIstuff and results in no references beign made in PMI
-	RoseDesign *geo = pnew RoseDesign;
-	std::string geoname(des->name());
-	geoname.append("_Geometry");
-	geo->name(geoname.c_str());
-	geo->save();
-	//copy_schema(geo, PMI);
-	//copy_header(geo, PMI);
-	stix_tag_units(PMI);
-	ARMpopulate(PMI);
-//#############################################################
-
-	//STModule
-	ARMCursor cur; //arm cursor
-	ARMObject *a_obj;
-	cur.traverse(PMI);
-	
-	Workpiece_IF  *workpiece = NULL;
-
-	ListOfRoseObject aimObjs;
-	rose_mark_begin();
-	rose_compute_backptrs(PMI);
-//Creates all references that may be necessary
-	while ((a_obj = cur.next())) {
-		
-		workpiece = a_obj->castToWorkpiece_IF();
-		if (workpiece) {
-			aimObjs.emptyYourself();
-			unsigned i, sz;
-			RoseObject * aimObj;
-			a_obj->getAIMObjects(&aimObjs);
-			ARMresolveReferences(&aimObjs);
-			//rose_compute_backptrs(PMI);
-			for (i = 0, sz = aimObjs.size(); i < sz; i++){
-				aimObj = aimObjs[i];
-			
-				//moves evyerthing
-				aimObj->move(geo, 0);
-
-				//std::cout << "marked " << aimObj->domain()->name() << " size: " << parents.size() << std::endl;}
-				
-			}
-		}
-	}
-
 	RoseObject *obj;
 	RoseCursor curse;
-	curse.traverse(PMI);
+	curse.traverse(source);
 	curse.domain(ROSE_DOMAIN(RoseStructure));
 	while (obj = curse.next())
 	{
 		handleEntity(obj);
 	}
-	rose_compute_backptrs(PMI);
-	rose_compute_backptrs(geo);
-	curse.traverse(geo);
+	rose_compute_backptrs(source);
+	rose_compute_backptrs(destination);
+	curse.traverse(destination);
 	curse.domain(ROSE_DOMAIN(RoseObject));
 	ListOfRoseObject Parents;
 	while (obj = curse.next())
-	{	
+	{
 		Parents.emptyYourself();
 		obj->usedin(NULL, NULL, &Parents);
 		if (Parents.size() == 0)
 		{
-			addRefAndAnchor(obj, geo, PMI);
+			addRefAndAnchor(obj, destination, source);
 		}
 	}
-	update_uri_forwarding(PMI);
+}
 
-	rose_mark_end();
+void MoveGeometry(RoseDesign * source, RoseDesign * dest)
+{
+	ARMCursor cur; //arm cursor
+	ARMObject *a_obj;
+	cur.traverse(source);
 
-	ARMgc(PMI);
-	ARMgc(geo);
-	rose_empty_trash();
+	Workpiece_IF  *workpiece = NULL;
 
-	geo->save();
-	ARMsave(geo);
-
-	PMI->save();	
-	ARMsave(PMI);
-	
-	return 0;
+	ListOfRoseObject aimObjs;
+	rose_mark_begin();
+	//Creates all references that may be necessary
+	while ((a_obj = cur.next()))
+	{
+		workpiece = a_obj->castToWorkpiece_IF();
+		if (workpiece)
+		{
+			aimObjs.emptyYourself();
+			unsigned i, sz;
+			RoseObject * aimObj;
+			a_obj->getAIMObjects(&aimObjs);
+			ARMresolveReferences(&aimObjs);
+			for (i = 0, sz = aimObjs.size(); i < sz; i++)
+			{
+				aimObj = aimObjs[i];
+				//moves evyerthing
+				aimObj->move(dest, 0);
+			}
+		}
+	}
 }
